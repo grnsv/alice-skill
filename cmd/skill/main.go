f9c4a3d06f379b1ff1496ee6f98b10b7ca0018fc
grnsv <grin.sv@gmail.com>
2025-01-16 00:30:33 +0300

    Added logs

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -1,14 +1,13 @@
-// пакеты исполняемых приложений должны называться main
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/grnsv/alice-skill/internal/logger"
+	"go.uber.org/zap"
 )
 
-// функция main вызывается автоматически при запуске приложения
 func main() {
-	// обрабатываем аргументы командной строки
 	parseFlags()
 
 	if err := run(); err != nil {
@@ -16,29 +15,31 @@ func main() {
 	}
 }
 
-// функция run будет полезна при инициализации зависимостей сервера перед запуском
 func run() error {
-	fmt.Println("Running server on", flagRunAddr)
-	return http.ListenAndServe(flagRunAddr, http.HandlerFunc(webhook))
+	if err := logger.Initialize(flagLogLevel); err != nil {
+		return err
+	}
+
+	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
+	// оборачиваем хендлер webhook в middleware с логированием
+	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(webhook))
 }
 
-// функция webhook — обработчик HTTP-запроса
 func webhook(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		// разрешаем только POST-запросы
+		logger.Log.Debug("got request with bad method", zap.String("method", r.Method))
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
-	// установим правильный заголовок для типа данных
 	w.Header().Set("Content-Type", "application/json")
-	// пока установим ответ-заглушку, без проверки ошибок
 	_, _ = w.Write([]byte(`
-      {
-        "response": {
-          "text": "Извините, я пока ничего не умею"
-        },
-        "version": "1.0"
-      }
-    `))
+		{
+		"response": {
+			"text": "Извините, я пока ничего не умею"
+		},
+		"version": "1.0"
+		}
+	`))
+	logger.Log.Debug("sending HTTP 200 response")
 }
